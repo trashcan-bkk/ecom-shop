eb159ddb7585b2175fc125bd483bca07ce679b1f
panitarn <panitarn.work@gmail.com>
2023-01-09 12:13:18 +0700

    Paging, filtering, sorting and searching

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,11 @@ namespace API
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +47,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
 
